8e2660d79cafecd2c950707eba8e4148a6bb5dc0
RENO <105489002+Gifted12@users.noreply.github.com>
2026-06-11 05:00:53 +0100

    Remove Google Fonts configure
    Removed Google Fonts configuration from nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  googlefont: [
-    {protocol:"https"},
-    {hostname:"fonts.googleapis.com"}
-  ]
+  
   // reactCompiler: true,
 };
 
